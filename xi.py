28b0bb26f67dce6e49485db0e0dec0916712a693
Christofer Ohlsson <christofer.ohlsson@booking.com>
2020-04-15 17:39:33 +0200

    so it begins

---
+++ xi.py
@@ -0,0 +1,16 @@
+class Player:
+
+    def __init__(self, name, nationality, clubs, positions):
+        this.name = name
+        this.nationality = nationality
+        this.clubs = clubs
+        this.positions = positions
+
+
+players = [
+    Player('Ibhrahimovic', 'Sweden', ['Ajax', 'Juventus', 'Inter', 'Barcelona', 'Milan', 'PSG'], ['ST']),
+    Player('Larsson', 'Sweden', ['Feyenoord', 'Celtic', 'Barcelona', 'Man U'], ['ST']),
+    Player('Ljungberg', 'Sweden', ['Arsenal'], ['LM', 'CM', 'RM']),
+    Player('Andersson', 'Sweden', ['Mönchengladbach', 'Blackburn', 'Bayern', 'Barcelona'], ['CB']),
+    Player('Schmeichel', 'Denmark', ['Man U'], ['GK']),
+]
