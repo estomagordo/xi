61636180eaa7b28766cb8f724cd2509baf1382b9
Christofer Ohlsson <christofer.ohlsson@booking.com>
2020-04-15 20:01:03 +0200

    going places

--- xi.py
+++ xi.py
@@ -37,6 +37,15 @@ argentina = Nation('Argentina')
 serbia = Nation('Serbia')
 croatia = Nation('Croatia')
 czechia = Nation('Czechia')
+romania = Nation('Romania')
+bulgaria = Nation('Bulgaria')
+slovenia = Nation('Slovenia')
+senegal = Nation('Senegal')
+egypt = Nation('Egypt')
+scotland = Nation('Scotland')
+gabon = Nation('Gabon')
+poland = Nation('Poland')
+algeria = Nation('Algeria')
 
 malmo = Club('Malmö')
 liverpool = Club('Liverpool')
@@ -69,8 +78,38 @@ blackburn = Club('Blackburn')
 willem = Club('Willem II')
 fulham = Club('Fulham')
 rostock = Club('Hansa Rostock')
+dc = Club('DC United')
+chicago = Club('Chicago')
+parma = Club('Parma')
+brescia = Club('Brescia')
+stuttgart = Club('Stuttgart')
+padova = Club('Padova')
+bologna = Club('Bologna')
+fiorentina = Club('Fiorentina')
+vicenca = Club('Vicenza')
+sampdoria = Club('Sampdoria')
+southampton = Club('Southampton')
+groningen = Club('Groningen')
+lille = Club('Lille')
+schalke = Club('Schalke 04')
+benfica = Club('Benfica')
+ny = Club('New York')
+monaco = Club('Monaco')
+la = Club('Los Angeles Galaxy')
+wolfsburg = Club('Wolfsburg')
+bremen = Club('Werder Bremen')
+lens = Club('Lens')
+bordeaux = Club('Bordeaux')
+cannes = Club('Cannes')
+sporting = Club('Sporting')
+fenerbahce = Club('Fenerbahce')
+saint_etienne = Club('Saint Etienne')
+leicester = Club('Leicester')
+le_havre = Club('Le Havre')
 
 st = Position('Striker')
+lw = Position('Left wing')
+rw = Position('Right wing')
 lm = Position('Left midfield')
 cm = Position('Center midfield')
 rm = Position('Right midfield')
@@ -85,5 +124,38 @@ players = [
     Player('Andersson', sweden, { malmo, monchengladbach, blackburn, bayern, barcelona }, [cb]),
     Player('Schmeichel', denmark, { man_u }, [gk]),
     Player('Hyypiä', finland, { willem, liverpool, leverkusen }, [cb]),
-    Player('Litmanen', finland, { fulham, malmo, rostock, ajax, liverpool, barcelona }, [st, cm])
+    Player('Litmanen', finland, { fulham, malmo, rostock, ajax, liverpool, barcelona }, [st, cm]),
+    Player('Michael Laudrup', denmark, { ajax, real_madrid, barcelona, juventus, lazio }, [cm]),
+    Player('Stoichkov', bulgaria, { dc, chicago, barcelona, parma }, [lw, st]),
+    Player('Hagi', romania, { barcelona, real_madrid, brescia }, [lm, cm]),
+    Player('Lahm', germany, { stuttgart, bayern }, [rb]),
+    Player('Maldini', italy, { milan }, [lb, cb]),
+    Player('Del Piero', italy, { juventus, padova }, [st, cm]),
+    Player('Baggio', italy, { milan, inter, brescia, bologna, vicenca, fiorentina, juventus }, [cm, st]),
+    Player('Gullit', netherlands, { milan, chelsea, sampdoria, psv, feyenoord }, [cm, rw]),
+    Player('van Basten', netherlands, { milan, ajax }, [st]),
+    Player('van Dijk', netherlands, { southampton, liverpool, celtic, groningen }, [cb]),
+    Player('Buffon', italy, { juventus, parma, psg }, [gk]),
+    Player('Hazard', belgium, { chelsea, lille, real_madrid }, [lw, cm, rw]),
+    Player('Neuer', germany, { bayern, schalke }, [gk]),
+    Player('Oblak', slovenia, { benfica, atletico_madrid }, [gk]),
+    Player('Henry', france, { ny, barcelona, arsenal, juventus, monaco }, [st, lw]),
+    Player('Sterling', england, { liverpool, man_c }, [lw, rw, st]),
+    Player('Ronaldo', brazil, { milan, real_madrid, inter, barcelona, psv }, [st]),
+    Player('Mané', senegal, { liverpool, southampton }, [lw, st, rw]),
+    Player('Salah', egypt, { liverpool, chelsea, roma, fiorentina }, [lw, st, rw]),
+    Player('Gerrard', england, { liverpool, la }, [cm]),
+    Player('de Bruyne', belgium, { man_c, chelsea, bremen, wolfsburg }, [cm]),
+    Player('Messi', argentina, { barcelona }, [st, rw]),
+    Player('Alexander-Arnold', england, { liverpool }, [rb]),
+    Player('Robertson', scotland, { liverpool }, [lb]),
+    Player('Varane', france, { lens, real_madrid }, [cb]),
+    Player('Zidane', france, { cannes, bordeaux, juventus, real_madrid }, [cm, lm]),
+    Player('Cristiano Ronaldo', portugal, { man_u, sporting, real_madrid, juventus }, [lw, st, rw]),
+    Player('Fabinho', brazil, { liverpool, monaco }, [cm]),
+    Player('Cafu', brazil, { milan, roma }, [rb, rm]),
+    Player('Roberto Carlos', brazil, { fenerbahce, real_madrid, inter }, [lb]),
+    Player('Aubameyang', gabon, { saint_etienne, arsenal, dortmund, milan, monaco, lille }, [lw, st, rw]),
+    Player('Lewandowski', poland, { bayern, dortmund }, [st]),
+    Player('Mahrez', algeria, { leicester, man_c, le_havre }, [rw, rm, cm]),
 ]
